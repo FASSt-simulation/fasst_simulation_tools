df5b159297355492bc78bfb3d114438db36f88e8
Shawn P. Serbin <sserbin@bnl.gov>
2022-10-03 17:02:24 -0400

    Updated met_scripts/ngeearctic/download_elm_singlesite_forcing_data.sh to remove old data before re-downloading new data

--- met_scripts/ngeearctic/download_elm_singlesite_forcing_data.sh
+++ met_scripts/ngeearctic/download_elm_singlesite_forcing_data.sh
@@ -27,6 +27,9 @@ mkdir -p ${cesm_data_dir}
 echo "*** Downloading and extracting forcing data ***"
 
 cd ${cesm_data_dir}
+# remove any old files before downloading updated met data
+rm -rf *
+# download the met data
 wget https://github.com/fmyuan/pt-e3sm-inputdata/archive/refs/heads/master.zip
 unzip master.zip
 cd pt-e3sm-inputdata-master
